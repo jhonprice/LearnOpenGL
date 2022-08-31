6d424249468a1868c19e4ffd6b38b13ab2428a74
因幡组员13号 <madhatter1800@qq.com>
2022-08-31 12:03:44 +0800

    配置assimp

---
+++ NewStarter/NewStarter.cpp
@@ -0,0 +1,107 @@
+#include<iostream>
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <stb_image.h>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+using namespace std;
+static int width = 600, height = 600;
+
+
+void testFunc() {
+	glm::vec4 a{1,1,1,1};
+	Assimp::Importer importer;
+	aiMesh* mesh;
+	aiScene* scene;
+}
+//<<渲染函数>>
+static void display(GLFWwindow* window)
+{
+	glfwSwapBuffers(window);
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+//<<设置视口>>
+static void reshape(GLFWwindow* window, int w, int h)
+{
+	width = w > 1 ? w : 1;
+	height = h > 1 ? h : 1;
+	glViewport(0, 0, width, height);
+}
+//<<处理输入>>
+static void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+int main(int argc, char** argv)
+{
+
+	//<<初始化GLFW>>
+#pragma region InitGLFW
+	auto initValue = glfwInit();
+	if (!initValue) std::cerr << "GLFW FAILED" << std::endl;
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#pragma endregion
+
+
+	//<<创建窗口>>
+#pragma region InitWindows
+	GLFWwindow* window;
+	window = glfwCreateWindow(width, height, "LearnOpenGL", nullptr, nullptr);
+	if (window == NULL)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+#pragma endregion
+
+
+	//<<初始化GL3W>>
+#pragma region InitGl3W
+	if (gl3wInit()) {
+		std::cerr << "failed to initialize OpenGL" << std::endl;
+		return -1;
+	}
+	if (!gl3wIsSupported(3, 3)) {
+		std::cerr << "OpenGL 3.3 not supported" << std::endl;
+		return -1;
+	}
+	std::cerr << "OpenGL: " << glGetString(GL_VERSION)
+		<< " GLSL: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+#pragma endregion
+
+
+	//<<视口设置>>
+	glfwSetFramebufferSizeCallback(window, reshape);
+
+
+
+	//<<渲染循环>>
+#pragma region RenderPart
+	while (!glfwWindowShouldClose(window)) {
+
+		//<<渲染函数>>
+		display(window);
+		//<<事件触发器>>
+		glfwPollEvents();
+		//<<处理输入>>
+		processInput(window);
+	}
+#pragma endregion
+
+
+
+	glfwTerminate();
+	return 0;
+}
