ee725e8da3495834dd7b9f359d5f1405690bb969
因幡组员13号 <madhatter1800@qq.com>
2022-09-05 21:39:33 +0800

    Final Project 框架搭建

---
+++ FinalProject/FinalProject.cpp
@@ -0,0 +1,137 @@
+#include<iostream>
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
+#include "game.h"
+#include "resource_manager.h"
+
+using namespace std;
+// The Width of the screen
+const unsigned int SCREEN_WIDTH = 800;
+// The height of the screen
+const unsigned int SCREEN_HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow*, int width, int height);
+void key_callback(GLFWwindow* window, int key, int, int action, int);
+
+Game Breakout(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+
+int main(int argc, char** argv)
+{
+
+	//<<初始化GLFW>>
+#pragma region InitGLFW
+	auto initValue = glfwInit();
+	if (!initValue) std::cerr << "GLFW FAILED" << std::endl;
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#pragma endregion
+
+
+	//<<创建窗口>>
+#pragma region InitWindows
+	GLFWwindow* window;
+	window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Breakout", nullptr, nullptr);
+	if (window == NULL)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+#pragma endregion
+
+
+	//<<初始化GL3W>>
+#pragma region InitGl3W
+	if (gl3wInit()) {
+		std::cerr << "failed to initialize OpenGL" << std::endl;
+		return -1;
+	}
+	if (!gl3wIsSupported(3, 3)) {
+		std::cerr << "OpenGL 3.3 not supported" << std::endl;
+		return -1;
+	}
+	std::cerr << "OpenGL: " << glGetString(GL_VERSION)
+		<< " GLSL: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+#pragma endregion
+
+
+	//<<视口设置>>
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+	// OpenGL configuration
+	// --------------------
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	// initialize game
+	// ---------------
+	Breakout.Init();
+
+
+	// deltaTime variables
+	// -------------------
+	float deltaTime = 0.0f;
+	float lastFrame = 0.0f;
+
+	//<<渲染循环>>
+#pragma region RenderPart
+	while (!glfwWindowShouldClose(window)) {
+		// calculate delta time
+		// --------------------
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+		glfwPollEvents();
+
+		// manage user input
+		// -----------------
+		Breakout.ProcessInput(deltaTime);
+
+		// update game state
+		// -----------------
+		Breakout.Update(deltaTime);
+
+		// render
+		// ------
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		Breakout.Render();
+
+		glfwSwapBuffers(window);
+	}
+#pragma endregion
+
+
+	ResourceManager::Clear();
+	glfwTerminate();
+	return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int, int action, int)
+{
+	// when a user presses the escape key, we set the WindowShouldClose property to true, closing the application
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+			Breakout.Keys[key] = true;
+		else if (action == GLFW_RELEASE)
+			Breakout.Keys[key] = false;
+	}
+}
+
+void framebuffer_size_callback(GLFWwindow*, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
