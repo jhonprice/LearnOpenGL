291a49857507028a8679b7ddd7109ea92da2737e
因幡组员13号 <madhatter1800@qq.com>
2022-09-05 22:13:08 +0800

    构建渲染精灵，FinalProject更新

--- FinalProject/game.cpp
+++ FinalProject/game.cpp
@@ -1,4 +1,10 @@
 #include "game.h"
+#include "resource_manager.h"
+#include "sprite_render.h"
+
+
+// Game-related State data
+SpriteRenderer* Renderer;
 
 Game::Game(unsigned int width, unsigned int height)
     : State(GAME_ACTIVE), Keys(), Width(width), Height(height)
@@ -8,12 +14,22 @@ Game::Game(unsigned int width, unsigned int height)
 
 Game::~Game()
 {
-
+    delete Renderer;
 }
 
 void Game::Init()
 {
-
+    ResourceManager::LoadShader("../shader/final_shader.vert", "../shader/final_shader.frag", nullptr, "sprite");
+    // configure shaders
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(this->Width),
+        static_cast<float>(this->Height), 0.0f, -1.0f, 1.0f);
+    ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
+    ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
+    // set render-specific controls
+    auto shader = ResourceManager::GetShader("sprite");
+    Renderer = new SpriteRenderer(shader);
+    // load textures
+    ResourceManager::LoadTexture("../image/awesomeface.png", true, "face");
 }
 
 void Game::Update(float dt)
@@ -28,5 +44,6 @@ void Game::ProcessInput(float dt)
 
 void Game::Render()
 {
-
+    auto texture = ResourceManager::GetTexture("face");
+    Renderer->DrawSprite(texture, glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 }
